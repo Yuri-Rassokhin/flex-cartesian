f27775318a11b29d73e17e39271267de777ab7ef
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-04-07 10:36:01 +0000

    enhanced: examples

--- examples/11_visualization.rb
+++ examples/11_visualization.rb
@@ -14,8 +14,4 @@ space = FlexCartesian.new(source: :xlsx, uri: "#{name}.xlsx", dimensions:
 space.func(:add, :inference) { |v| space.data(:get, vector: v, target: "collect_inference_time") }
 space.func(:run)
 
-analyzer = space.analyzer(:morris, trajectories: 1000, step: 0.1, seed: 31)
-
-analyzer.output(function: :inference, colorize: true, format: :markdown)
-
 space.visualize(format: :html, x: :iterate_requests, y: :iterate_processes, function: :inference, output: "viz.html")
