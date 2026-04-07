8e80faaf5a84842149d340fa6392695b30f03961
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-04-07 11:55:24 +0000

    polished: integrated sensitivity table on landing

--- examples/09_data/09_data.rb
+++ examples/09_data/09_data.rb
@@ -1,6 +1,6 @@
 require 'flex-cartesian'
 
-space = FlexCartesian.new(source: :xlsx, uri: "./examples/09_visualization/yolo-arm-a1.xlsx", dimensions: [:iterate_iteration, :infra_shape, :iterate_requests, :iterate_processes] )
+space = FlexCartesian.new(source: :xlsx, uri: "./examples/09_data/yolo-arm-a1.xlsx", dimensions: [:iterate_iteration, :infra_shape, :iterate_requests, :iterate_processes] )
 
 space.func(:add, :inference) { |v| space.data(:get, vector: v, target: "collect_inference_time") }
 space.func(:run, progress: true, title: "Obtaining AI data")
