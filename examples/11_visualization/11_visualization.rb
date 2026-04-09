7e89a3a6d28bbf20d6fb03f525ec845ee5ce037a
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-04-09 20:44:19 +0000

    enhanced: viz example

--- examples/11_visualization/11_visualization.rb
+++ examples/11_visualization/11_visualization.rb
@@ -1,9 +1,22 @@
 require 'flex-cartesian'
 
-space = FlexCartesian.new(source: :xlsx, uri: "./examples/11_visualization/yolo-arm-a1.xlsx", dimensions: [:iterate_iteration, :infra_shape, :iterate_requests, :iterate_processes] )
+source = "./examples/11_visualization/yolo-arm-a1.xlsx"
+
+space = FlexCartesian.new(source: :xlsx, uri: source, dimensions: [:iteration, :requests, :processes] )
 
 space.func(:add, :inference) { |v| space.data(:get, vector: v, target: "collect_inference_time") }
 space.func(:run)
-space.cond(:set) { |v| space.function(v, :inference) <= 10 }
+#space.cond(:set) { |v| space.function(v, :inference) <= 10 }
 
-space.visualize(format: :html, x: :iterate_requests, y: :iterate_processes, function: :inference, output: "./examples/11_visualization/viz.html", show_legend: false, show_z_title: false, show_grid: true, equal_axes: true, start_at_zero: true)
+space.visualize(
+  format: :html,
+  x: :requests,
+  y: :processes,
+  function: :inference,
+  output: "./examples/11_visualization/viz.html",
+  show_legend: false,
+  show_z_title: false,
+  show_grid: true,
+  equal_axes: true,
+  start_at_zero: true
+)
