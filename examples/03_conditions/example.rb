9ef58594b766a983e63841fbafcd39b6511324c3
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-04-18 15:15:36 +0000

    debugged: example 3

--- examples/03_conditions/example.rb
+++ examples/03_conditions/example.rb
@@ -6,24 +6,24 @@ example = {
   dim2: ['x', 'y'],
   dim3: [true, false]
 }
-s = FlexCartesian.new(example)
+space = FlexCartesian.new(example)
 
 # Add condition that allows only combinations where value in the 1st dimension is odd
-s.cond(:set) { |v| v.dim1.odd? }
+space.cond(:set) { |v| v.dim1.odd? }
 # Print the conditions in the parameter space
-s.cond
+space.cond
 # Print the parameter space with the condition applied, and show the size of Cartesian space after applying the condition
-s.output
+space.output
 # See the size of parameter space changed by the condition
-puts "Size with conditions: #{s.size}"
+puts "Size with conditions: #{space.size}"
 # Remove the condition using its index in order of setting conditions, starting from zero
-s.cond(:unset, index: 0)
+space.cond(:unset, index: 0)
 # Remove all conditions, if any
-s.cond(:clear)
+space.cond(:clear)
 # See the size of parameter space restored to default size of the full space
-puts "Restored size of full space: #{s.size}"
+puts "Restored size of full space: #{space.size}"
 # Set condition based on function result - highly useful for isosurfaces and such
 space.func(:add, :example) { |v| v.dim1 + dim3 ? 2 : 3 }
 space.cond(:set) { |v| space.function(v, :example) <= 3 }
-s.output
+space.output
 
