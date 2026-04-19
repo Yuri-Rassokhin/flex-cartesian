d55c23c1e12a66c75621ecf0c2032c8970e52da3
Yuri Rassokhin <yuri.rassokhin@gmail.com>
2026-04-19 17:40:47 +0000

    updated: README

--- examples/13_chatgpt_semantic_shift/example.rb
+++ examples/13_chatgpt_semantic_shift/example.rb
@@ -26,9 +26,6 @@ space.func(:run, progress: true, title: "Obtaining behavioural data")
 # parameter space with behavioral functions is the behavioural model - we can visualize it
 space.visualize(x: :temperature, y: :tokens, func: :semantic_shift, output: "./examples/13_chatgpt_semantic_shift/viz.html")
 
-puts "Visualization saved to ./examples/13_chatgpt_semantic_shift/viz.html"
-
 # finally, analyze how precisely parameters influence ChatGPT's response
-a = space.analyzer(:morris, trajectories: 10, step: 0.1, seed: 42)
-a.output(colorize:true, func: :semantic_shift)
+space.analyzer(:morris, trajectories: 10, step: 0.1, seed: 42).output(func: :semantic_shift, format: :markdown)
 
